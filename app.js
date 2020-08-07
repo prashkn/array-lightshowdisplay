390107ee3349e6a41657b367fbfe7c9a8ab4b691
Prashant Naganaboyina <pnaganab@gmail.com>
2020-08-07 01:19:52 -0400

    allow user input on the number of lights

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //let the forloop go thru the array of lights and turn on the lights of one at a time till a certain point.
 //user can control amount of lights and speed
 
+//to do:
+//work on the lighting up and speed part of it
 
+var lights = document.getElementById("lightHolder");
 let lightArray = [];
 
 //slider stuff for amount of lights
@@ -10,6 +13,8 @@ var numberOfLightsOutput = document.getElementById("numberOfLights");
 numberOfLightsOutput.innerHTML = slider.value; // Display the default slider value
 slider.oninput = function() {
   numberOfLightsOutput.innerHTML = this.value;
+  lights.innerHTML = "";
+  showLights();
 }
 
 //slider stuff for speed
@@ -18,4 +23,11 @@ var speedOutput = document.getElementById("speed");
 speedOutput.innerHTML = speedSlider.value; // Display the default slider value
 speedSlider.oninput = function() {
   speedOutput.innerHTML = this.value;
+}
+
+
+function showLights(){
+  for (var num = 0; num < slider.value; num++) {
+    lights.innerHTML += '<img class="lights" src="lightbulb.png" style="height: 50px;">';
+  }
 }
